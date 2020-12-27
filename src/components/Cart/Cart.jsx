198d230a47a5a7bc99227fd434b6627c3c83afe0
rafmasloman <raflymasloman20@gmail.com>
2020-12-27 19:57:24 +0800

    Menambah Componen Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,17 @@
 import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import "./css/Cart.css";
 class Cart extends Component {
   render() {
     return (
       <section className="cart-container">
-        {console.log(this.props.getCounter.count)}
-        <div className="cart-container">
-          <FontAwesomeIcon icon={faCartArrowDown} />
+        <div className="cart-hero">
+          {/* {this.props.id()} */}
           <h1>{this.props.getCounter.count}</h1>
+          <a onClick={this.props.shoesId}>
+            <FontAwesomeIcon icon={faCartPlus} className="font-awesome-icon" />
+          </a>
         </div>
       </section>
     );
